3ddae41e75302b8243d416e3bd9754bccd6f0184
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-12 17:56:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['frontendcleservice.onrender.com']
+  build: {
+    rollupOptions: {
+      external: ['jquery']
+    }
   }
 });
