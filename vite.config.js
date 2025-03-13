2a306987629afe5bfca1ba0a6923d4c656178a43
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-13 08:43:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // other configurations…
-  preview: {
-    allowedHosts: ['frontendcleservice.onrender.com']
+  build: {
+    rollupOptions: {
+      external: ['jquery']
+    }
   }
-})
+});
