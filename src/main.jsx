9ad94b85a3fff18e4d71d94834af1990b6aa798c
alexis bouvet <alexisdashlane@gmail.com>
2025-03-13 09:43:20 +0100

    Revert "Votre message de commit"
    This reverts commit 4e36a80de95b1315f902b7dda9ec0b183ace9e14, reversing
    changes made to 97bf4d94097b9e7b5560a7254c44a05193f05ee7.
    
    git pull --rebase
    git revert --continue

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
+<<<<<<< HEAD
 // index.js ou App.jsx (avant le rendu React)
 import $ from 'https://code.jquery.com/jquery-3.6.0.min.js';
 
 window.$ = window.jQuery = $;
 
 
+=======
+>>>>>>> parent of 4e36a80 (Votre message de commit)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,15 +15,12 @@ import App from './SiteWeb/App';
 import { theme } from './theme';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  console.error("L'élément racine n'a pas été trouvé");
-} else {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  );
-}
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+);
