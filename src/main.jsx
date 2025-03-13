917a4cf9b1588d9e42ed6b456994a0c6a8ebe366
alexis bouvet <alexisdashlane@gmail.com>
2025-03-13 10:12:29 +0100

    Votre message de commit ici

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import App from './SiteWeb/App';
 import { theme } from './theme';
 
+const Main = () => {
+  useEffect(() => {
+    // Exemple d'utilisation de jQuery :
+    // On fait disparaître l'élément avec l'id "root" puis on le fait apparaître en fondu sur 1 seconde.
+    $('#root').hide().fadeIn(1000);
+  }, []); // Ce code s'exécute une seule fois au montage du composant
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+};
+
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
-);
+root.render(<Main />);
