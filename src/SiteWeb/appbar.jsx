52a4ead73478058ecbd27cf245cd3f6c8766b87a
alexis bouvet <alexisdashlane@gmail.com>
2025-03-14 11:17:04 +0100

    Votre message de commit

--- src/SiteWeb/appbar.jsx
+++ src/SiteWeb/appbar.jsx
@@ -178,9 +178,48 @@ const Header = () => {
           textAlign: 'center',
         }}
       >
-        <Typography variant="h5" sx={{ fontWeight: 600, ...gradientText }}>
-          Bienvenue sur Cleservice.com
-        </Typography>
+        <Typography
+          component="h2"
+          gutterBottom
+          sx={{
+            fontFamily: 'Lato, sans-serif',
+            fontWeight: '700',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: { xs: '1rem', md: '1.1rem' },
+            color: "#e0e0e0",
+            py: 0,
+          }}
+        >
+          Si vous avez des questions ou besoin d'assistance pour passer commande, appelez le&nbsp;
+          <Box
+            component="a"
+            href="tel:0142674861"
+            sx={{
+              color: 'red',
+              fontFamily: 'Lato, sans-serif',
+              position: 'relative',
+              display: 'inline-block',
+              overflow: 'hidden',
+              textDecoration: 'none',
+              padding: '0 2px',
+              '&::after': {
+                content: '""',
+                position: 'absolute',
+                top: 0,
+                left: '-100%',
+                width: '100%',
+                height: '100%',
+                background: 'linear-gradient(120deg, transparent, rgba(255,255,255,0.8), transparent)',
+                animation: 'laser 2s linear infinite',
+                borderRadius: '2px',
+              },
+            }}
+          >
+            01 42 67 48 61
+          </Box>
+          </Typography>
       </Box>
 
       
