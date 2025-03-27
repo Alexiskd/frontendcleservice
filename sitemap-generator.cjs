178e4be0a1fcc641a806f39902fb4098f37a2f8b
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-27 12:04:00 +0100

    Update sitemap-generator.cjs

--- sitemap-generator.cjs
+++ sitemap-generator.cjs
@@ -1,7 +1,7 @@
 const path = require('path');
 const Sitemap = require('react-router-sitemap').default;
 // Importez ou définissez vos routes. Par exemple, si vos routes sont dans src/App.js :
-const router = require('./src/App').default; 
+const router = require('./src/siteweb/App').default; 
 
 function generateSitemap() {
   return (
