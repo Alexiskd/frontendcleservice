8b4a9bfff7ef40403ea477b686a3876f8336a960
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-27 12:27:14 +0100

    Update sitemap-generator.cjs

--- sitemap-generator.cjs
+++ sitemap-generator.cjs
@@ -1,16 +1,23 @@
+const fs = require('fs');
 const path = require('path');
-const Sitemap = require('react-router-sitemap').default;
-// Importez ou définissez vos routes. Par exemple, si vos routes sont dans src/App.js :
-const router = require('./src/SiteWeb/App.jsx').default;
+const routes = require('./routes.js');
 
+const baseUrl = 'http://cleservice.com';
+let sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+sitemapXml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" `;
+sitemapXml += `xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" `;
+sitemapXml += `xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 `;
+sitemapXml += `http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n`;
 
-function generateSitemap() {
-  return (
-    new Sitemap(router)
-      .build('https://frontendcleservice.onrender.com')
-      .save(path.resolve('./public/sitemap.xml'))
-  );
-}
+routes.forEach(route => {
+  sitemapXml += `  <url>\n`;
+  sitemapXml += `    <loc>${route.loc}</loc>\n`;
+  sitemapXml += `    <changefreq>${route.changefreq}</changefreq>\n`;
+  sitemapXml += `    <priority>${route.priority}</priority>\n`;
+  sitemapXml += `  </url>\n`;
+});
 
-generateSitemap();
+sitemapXml += `</urlset>`;
 
+fs.writeFileSync(path.resolve(__dirname, 'public/sitemap.xml'), sitemapXml, 'utf8');
+console.log('Sitemap généré avec succès.');
