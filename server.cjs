0980c9770021e1e8d3104561627d8eb9fae153b4
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-31 18:01:41 +0200

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,5 +1,10 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
