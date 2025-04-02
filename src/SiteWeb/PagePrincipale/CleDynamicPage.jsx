2759b59f4c2f5dda31b953d3d6a0269330731f58
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-02 10:10:11 +0200

    Update CleDynamicPage.jsx

--- src/SiteWeb/PagePrincipale/CleDynamicPage.jsx
+++ src/SiteWeb/PagePrincipale/CleDynamicPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react'; 
 import { Helmet } from 'react-helmet';
 import {
   Box,
