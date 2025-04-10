900999dce02795b93da721927f1a47064614095a
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-10 16:10:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -7,11 +6,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Option 1 : Si vos sources sont dans un dossier interne "src" et preloadData.js se trouve dans "src/utils"
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      
-      // Option 2 : Si vos sources sont directement dans /src/ (sans dossier interne)
-      // '@utils': path.resolve(__dirname, 'utils'),
+      '@PagePrincipale': path.resolve(__dirname, 'src/PagePrincipale'),
     },
   },
+  preview: {
+    allowedHosts: ['frontendcleservice.onrender.com', 'www.cleservice.com']
+  }
 });
