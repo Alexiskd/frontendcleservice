4cac745919195712edc0ee20c9120dea8990a57b
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-10 17:01:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@utils': '/src/utils',  // Veillez à ce que ce chemin corresponde à votre structure de projet
-    },
-  },
+  preview: {
+    allowedHosts: ['www.cleservice.com']
+  }
 });
