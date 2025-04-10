e67b8ebb604abcd2d8bc695671d2f7de64141876
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-10 09:49:24 +0200

    Update brandsApi.js

--- src/SiteWeb/brandsApi.js
+++ src/SiteWeb/brandsApi.js
@@ -1,4 +1,4 @@
-// @utils/preloadData.js
+// src/utils/preloadData.js
 
 /**
  * Précharge les clés (produits) associées à une marque spécifique depuis l'API.
@@ -7,21 +7,18 @@
  * @returns {Promise<Array>} - Une promesse qui se résout avec un tableau de clés ou, en cas d'erreur, un tableau vide.
  */
 export const preloadKeysData = async (brandName) => {
-  // Vérification que la marque a été spécifiée
   if (!brandName) {
     console.error("Le nom de la marque est requis pour précharger les données.");
     return [];
   }
-
-  // Déclaration d'une URL de base configurable (vous pouvez en faire une constante d'environnement par exemple)
+  
+  // Utilisez une constante pour l'URL de base (vous pouvez l'adapter ou la passer via un fichier d'environnement)
   const baseUrl = 'https://cl-back.onrender.com';
-  // Construction de l'endpoint à partir du nom de la marque
   const endpoint = `/produit/cles?marque=${encodeURIComponent(brandName)}`;
   const url = `${baseUrl}${endpoint}`;
 
   try {
     const response = await fetch(url);
-    // Vérification que la réponse du serveur est correcte
     if (!response.ok) {
       console.error(`Erreur: Le serveur a renvoyé le statut ${response.status} pour l'URL : ${url}`);
       throw new Error('Erreur lors du préchargement des clés');
