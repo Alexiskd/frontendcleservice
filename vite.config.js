b3f3763ff218593b2bdc4e8b66efa56533e46284
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-10 16:10:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Ici, l'alias "@utils" pointe vers "src/utils"
+      // Option 1 : Si vos sources sont dans un dossier interne "src" et preloadData.js se trouve dans "src/utils"
       '@utils': path.resolve(__dirname, 'src/utils'),
+      
+      // Option 2 : Si vos sources sont directement dans /src/ (sans dossier interne)
+      // '@utils': path.resolve(__dirname, 'utils'),
     },
   },
 });
