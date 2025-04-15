0686ba9f865b11267ff963884c2a3da1a4a3e841
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-15 09:55:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['www.cleservice.com'] // Ajoutez le ou les host autorisés
-  }
+  resolve: {
+    alias: {
+      '@api': '/src/api',
+    },
+  },
 });
