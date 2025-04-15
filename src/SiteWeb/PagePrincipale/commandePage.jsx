92ce732f2f473d80c0db83c8d4f7c55447e26a7a
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-15 16:47:15 +0200

    Update commandePage.jsx

--- src/SiteWeb/PagePrincipale/commandePage.jsx
+++ src/SiteWeb/PagePrincipale/commandePage.jsx
@@ -38,7 +38,7 @@ const CommandePage = () => {
   }, [name]);
 
   const handleCommander = () => {
-    // Redirection vers la page de finalisation de la commande en utilisant l'id du produit
+    // Rediriger vers la page de finalisation de commande en utilisant l'id du produit
     if (produit && produit.id) {
       navigate(`/finaliser-commande/${produit.id}`);
     }
