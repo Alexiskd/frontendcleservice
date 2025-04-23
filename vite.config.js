cd6328aa805838e3c0665374f8b43740259a86d4
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-23 10:11:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -6,18 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@api': path.resolve(__dirname, 'src/api'),
-      '@utils': path.resolve(__dirname, 'src/utils'), // Ajout de l'alias pour @utils
-      // … vos autres alias
-    }
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
   },
-  preview: {
-    // autorise explicitement ce host lors d'un `vite preview`
-    allowedHosts: ['www.cleservice.com']
-  },
-  build: {
-    rollupOptions: {
-      external: ['@utils/preloadData.js'], // Optionnel, si vous souhaitez externaliser ce module
-    }
-  }
 });
