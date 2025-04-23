613a62a8fed2089c928b2f9b4ee835f63f54991f
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-04-23 13:25:52 +0200

    Update commandePage.jsx

--- src/SiteWeb/PagePrincipale/commandePage.jsx
+++ src/SiteWeb/PagePrincipale/commandePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import axios from 'axios';
 import { CircularProgress, Grid, Typography, Box } from '@mui/material';
 
@@ -67,4 +67,5 @@ const CommandePage = () => {
 };
 
 export default CommandePage;
+
 
